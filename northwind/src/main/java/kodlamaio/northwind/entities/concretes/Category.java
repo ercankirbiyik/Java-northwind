257fac6b0e8cbbaf356ffdf5b58df3c3bc43231c
ercan kırbıyık <rcnkirbiyik@gmail.com>
2021-05-27 21:51:48 +0300

    northwind

--- northwind/src/main/java/kodlamaio/northwind/entities/concretes/Category.java
+++ northwind/src/main/java/kodlamaio/northwind/entities/concretes/Category.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
@@ -19,6 +20,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "categories")
 @Entity
 public class Category {
+	@Id
 	@Column(name = "category_id")
 	private int categoryId;
 	
